e76bd21e3c1c4771d22449f16745737ef1c99707
Momodayoo <msyauqi94@gmail.com>
2023-10-22 16:10:26 +0800

    deleted tets

--- tets.js
+++
@@ -1,8 +0,0 @@
-function sayHi() {
-    console.log('Hi');
-    sayHi.counter++; // increment the custom counter property every time the function is called
-    }
-    sayHi.counter = 4 // initialise the counter to 0 before calling it
-    sayHi() // Hi - calls the function and increments the counter
-    sayHi() // Hi - calls the function and increments the counter again
-    console.log( `Called ${sayHi.name} ${sayHi.counter} times` ) // Called sayHi 2 times
